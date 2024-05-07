fce1dcf6fd768d02cacf6b33da6a88ea51ea4306
DiyaKC123 <kcdipti8@gmail.com>
2024-05-07 17:15:12 +0545

    first commit

---
+++ JComboBoxExample.java
@@ -0,0 +1,43 @@
+import javax.swing.*; 
+import java.awt.event.*; 
+public class JComboBoxExample
+{ 
+JFrame f=new JFrame("JComboBox Demo");   
+JLabel l1 = new JLabel(); 
+JComboBox cb1;
+    void makeMyWindow()
+    { 
+ JLabel l2 = new JLabel("Select Your Birth Month"); 
+ l2.setBounds(50, 50, 200, 30); 
+ l1.setBounds(50, 300,200, 20); 
+ String months[]={"January", "February", "March", 
+ "April","May", "June", "July", "August", 
+ "September", "October", "November", "December"}; 
+
+ MyHandler h= new MyHandler(); 
+ cb1 =new JComboBox(months); 
+ cb1.setBounds(50, 100,90,20); 
+ cb1.addActionListener(h); 
+ 
+ f.add(cb1); 
+ f.add(l1);
+ f.add(l2); 
+ f.setLayout(null); 
+ f.setSize(400,400); 
+ f.setVisible(true); 
+ f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+} 
+ class MyHandler implements ActionListener 
+ { 
+ @Override 
+ public void actionPerformed(ActionEvent ae){ 
+ String m =(String)cb1.getSelectedItem(); 
+ l1.setText("Your Birth Month is "+m); 
+ }
+ } 
+ 
+public static void main(String[] args) { 
+ JComboBoxExample cmb= new JComboBoxExample(); 
+ cmb.makeMyWindow();
+}
+} 
